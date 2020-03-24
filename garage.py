cd13afc4da1418a6d0f46cbfce54867dc363f888
Ivan Davletshin <j.rooft@gmail.com>
2020-03-24 00:34:03 -0400

    wire to gpio

--- garage.py
+++ garage.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
+import RPi.GPIO as GPIO
+import time
+from concurrent.futures import ThreadPoolExecutor
 
-def open_close(acion: str) -> None:
-    if acion == 'open':
-        print('opening')
-    elif acion == 'close':
-        print('closing')
+
+GPIO.setmode(GPIO.BCM)
+garage_door_pin = 25
+GPIO.setup(garage_door_pin, GPIO.OUT)
+executor = ThreadPoolExecutor(max_workers=2)
+
+
+def open_close(action: str) -> None:
+    executor.submit(open_close_bg, (action))
+
+
+def open_close_bg(action: str) -> None:
+    # set to high to 500ms
+    GPIO.output(garage_door_pin, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(garage_door_pin, GPIO.LOW)
+
